0f345f9c25f3a22c98be96adcb40ce1a8fb91be8
amychow <13067802137@163.com>
2025-06-21 11:29:49 +0800

    init: init repo

---
+++ src/components/aside.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="aside-container"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <p class="logo-lg">DIDI陪诊</p>
+        <TreeMenu />
+      </el-menu>
+</template>
+
+<script setup lang="ts">
+   import TreeMenu from './treeMenu.vue'
+
+  const handleOpen = () =>{
+
+  }
+  const handleClose =() =>{
+
+  }
+</script>
+
+<style lang="less" scoped>
+ .aside-container {
+    height: 100%;
+    .logo-lg {
+        font-size: 20px;
+        text-align: center;
+        height: 50px;
+        line-height: 50px;
+        color: #fff;
+    }
+ }
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import {createRouter,createWebHashHistory } from 'vue-router'
+
+import Layout from '../views/Main.vue'
+import Login from '../views/login/index.vue'
+
+
+const routes = [
+    {
+        path:'/',
+        component:Layout
+    },
+    {
+        path:'/login',
+        component:Login
+    }
+]
+
+const router =  createRouter({
+    // 路由数据
+    routes: routes,
+    // 路由匹配模式
+    history:createWebHashHistory() 
+})
+
+export default router
